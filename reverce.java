97df24019e188226e18f95660f49bc43fe2920a1
Preetpal10 <preetpal0107@gmail.com>
2026-04-11 19:53:46 +0530

    change in code

--- reverce.java
+++ reverce.java
@@ -3,22 +3,27 @@ import java.util.Scanner;
 public class reverce {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter digit: " );
         int n = sc.nextInt();
         int temp = n;
+        int reverce = 0;
 
         while(n != 0 ){
             int remainder = n % 10;
-            int reverce = reverce * 10 + remainder;
+               reverce = reverce * 10 + remainder;
 
             n = n/10;
         }
     
-        if ( reverce = temp ){
+        
+        if ( reverce == temp ){
             System.out.println(" This is a palindrom");
         } else {
             System.out.println(" this is not palindrom");
         }
 
+            sc.close();
     }
     
 }
+  
