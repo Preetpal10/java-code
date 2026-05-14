7ffe51f4296890e348d492796339e2b264d1fc12
Preetpal10 <preetpal0107@gmail.com>
2026-05-14 22:48:51 +0530

    max and min arraylist

--- MagicNumber.java
+++ MagicNumber.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class MagicNumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a num: ");
         int num = sc.nextInt();
         int sum = num;
 
